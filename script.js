fa79ec06349a4fd9efe2e6d4bf564e5019116303
Andy <ander1386@gmail.com>
2022-08-24 16:34:55 -0500

    class04 finished

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+( () => {
 const btn = document.querySelector('[data-form-btn]');
 
 const createTask = (evento) => {
@@ -8,14 +9,33 @@ const createTask = (evento) => {
     const value = input.value;
     const list = document.querySelector('[data-list]');
     input.value = '';
-    const content = `<div>
-    <i class="far fa-check-square icon"></i>
-    <span class="task">${value}</span>
-    </div>
+    const taskContent = document.createElement('div');
+    const titleTask = document.createElement('span');
+    taskContent.appendChild(checkComplete());
+    taskContent.appendChild(titleTask);
+    titleTask.classList.add('task');
+    titleTask.innerHTML = value;
+    const content = `
     <i class="fas fa-trash-alt trashIcon icon"></i>`;
-    task.innerHTML = content;
+    // task.innerHTML = content;
+    task.appendChild(taskContent);
 
     list.appendChild(task);
 }
 
 btn.addEventListener('click', createTask);
+
+const checkComplete = () => {
+    const i = document.createElement('i');
+    i.classList.add('far', 'fa-check-square', 'icon');
+    i.addEventListener('click', completeTask);
+    return i;
+}
+
+const completeTask = (event) => {
+    const element = event.target;
+    element.classList.toggle('fas');
+    element.classList.toggle('completeIcon');
+    element.classList.toggle('far');
+}
+})();
