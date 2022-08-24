1af266086adb72a48bdfbdc760eb2ba0e09b8c00
Andy <ander1386@gmail.com>
2022-08-24 09:08:27 -0500

    class03 finished

--- script.js
+++ script.js
@@ -3,7 +3,19 @@ const btn = document.querySelector('[data-form-btn]');
 const createTask = (evento) => {
     evento.preventDefault();
     const input = document.querySelector('[data-form-input]');
-    console.log(input.value);
+    const task = document.createElement('li');
+    task.classList.add('card');
+    const value = input.value;
+    const list = document.querySelector('[data-list]');
+    input.value = '';
+    const content = `<div>
+    <i class="far fa-check-square icon"></i>
+    <span class="task">${value}</span>
+    </div>
+    <i class="fas fa-trash-alt trashIcon icon"></i>`;
+    task.innerHTML = content;
+
+    list.appendChild(task);
 }
 
 btn.addEventListener('click', createTask);
