c10e96fdc770e9fc0f76d184e1bc6a3bb70f8788
Andy <ander1386@gmail.com>
2022-08-25 10:55:18 -0500

    class06 calendar finished

--- script.js
+++ script.js
@@ -3,13 +3,21 @@
 
 const btn = document.querySelector('[data-form-btn]');
 
+const addTask = (evento) => {
+    const list = document.querySelector('[data-list]');
+    const task = createTask(evento);
+    list.appendChild(task);
+}
+
 const createTask = (evento) => {
     evento.preventDefault();
     const input = document.querySelector('[data-form-input]');
+    const calendar = document.querySelector('[data-form-date]');
     const task = document.createElement('li');
     task.classList.add('card');
     const value = input.value;
-    const list = document.querySelector('[data-list]');
+    const date = calendar.value;
+    const dateFormat = moment(date).format('DD/MM/YYYY');
     input.value = '';
     const taskContent = document.createElement('div');
     const titleTask = document.createElement('span');
@@ -19,9 +27,12 @@ const createTask = (evento) => {
     taskContent.appendChild(titleTask);
     
     // task.innerHTML = content;
+    const dateElement = document.createElement('span');
+    dateElement.innerHTML = dateFormat;
     task.appendChild(taskContent);
+    task.appendChild(dateElement);
     task.appendChild(deleteIcon());
-    list.appendChild(task);
+    return task;
 }
 
-btn.addEventListener('click', createTask);
+btn.addEventListener('click', addTask);
